2f24a63dff74904ffc6c766b0ad14df1c8afb8e4
Ethan Sanders <ethanmrcod@gmail.com>
2020-12-30 15:16:53 -0600

    updated my test code file to include clarification that it isn't currently complete.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StartingCodeChangeClassName.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StartingCodeChangeClassName.java
@@ -1,7 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
-//All that this is is starter code to base your TeleOp off of. You will need to import more libraries
-// to get this to work.
+//All that this is is starter code to base your TeleOp off of.
+// You will need to import more libraries to get this to work.
+// This will eventually include many more packages and all declarations for parts of the robot.
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
