15c57f8d951b94a99c8d7cc0235f43eaaadebb66
Ethan Sanders <ethanmrcod@gmail.com>
2021-02-05 19:44:25 -0600

    increased the standby motor value to 0.1 UNTESTED - DO NOT RUN WITH LIFT

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EthanMTestNotNihalNotPoggersTestas.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EthanMTestNotNihalNotPoggersTestas.java
@@ -39,7 +39,7 @@ public class EthanMTestNotNihalNotPoggersTestas extends LinearOpMode {
             telemetry.update();
             LeftStickValue = this.gamepad1.left_stick_y;
             if (LeftStickValue == 0) {
-                BigMotor.setPower(0.001);
+                BigMotor.setPower(0.1);
             }
             else {
                 BigMotor.setPower(LeftStickValue*0.5);
