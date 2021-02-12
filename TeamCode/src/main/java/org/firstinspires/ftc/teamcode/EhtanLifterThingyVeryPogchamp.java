0c0c492ab3184c4d7060caadaa4f0ed5b93cd954
Ethan Sanders <ethanmrcod@gmail.com>
2021-02-11 19:02:19 -0600

    trying the code again, it should actually function this time in theory

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EhtanLifterThingyVeryPogchamp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EhtanLifterThingyVeryPogchamp.java
@@ -12,34 +12,47 @@ public class EhtanLifterThingyVeryPogchamp extends LinearOpMode {
     private DcMotor LiftMotor;
     private CRServo ServoPower;
     private double LeftStickValue;
-    private class Lifter {
-        double LeftStickValue = gamepad1.left_stick_y;
-        double RightStickValue = gamepad1.right_stick_y;
+    private double RightStickValue;
 
-        private void MoveLift () {
-            if (LeftStickValue == 0) { // Do not complain about my use of multiple if statements or you will perish
+    private class Lifter {
+        double LeftStick = gamepad1.left_stick_y;
+        double RightStick = gamepad1.right_stick_y;
+
+        private void MoveLift() {
+            if (LeftStick == 0) { // Do not complain about my use of multiple if statements or you will perish
                 LiftMotor.setPower(0.09);
             } else {
-                if (LeftStickValue < 0) {
-                    LiftMotor.setPower(LeftStickValue * 0.55);
+                if (LeftStick < 0) {
+                    LiftMotor.setPower(LeftStick * 0.55);
                 } else {
-                    LiftMotor.setPower(LeftStickValue);
+                    LiftMotor.setPower(LeftStick);
                 }
             }
         }
-        private void MoveServo () {
-            ServoPower.setPower(RightStickValue);
+
+        private void MoveServo() {
+            ServoPower.setPower(RightStick);
         }
     }
 
 
     @Override
-        public void runOpMode () {
-            EhtanLifterThingyVeryPogchamp.Lifter lift = new EhtanLifterThingyVeryPogchamp.Lifter();
+    public void runOpMode() {
+        EhtanLifterThingyVeryPogchamp.Lifter lift = new EhtanLifterThingyVeryPogchamp.Lifter();
+        telemetry.addData("left stick y axis", LeftStickValue);
+        telemetry.update();
+        telemetry.addData("right stick y axis", RightStickValue);
+        telemetry.update();
+        expansion_Hub_1 = hardwareMap.get(Blinker.class, "Nihal");
+        LiftMotor = hardwareMap.get(DcMotor.class, "Big Motor");
+        ServoPower = hardwareMap.get(CRServo.class, "ServoPower");
+        waitForStart();
+        while (opModeIsActive()) {
             lift.MoveLift();
             lift.MoveServo();
-    }
+            telemetry.update();
+        }
 
 
     }
-
+}
