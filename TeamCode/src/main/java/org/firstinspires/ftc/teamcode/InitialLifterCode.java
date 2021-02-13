61ca0717d5d7b01f0b21083a4988bf5770ebd35c
Ethan Sanders <ethanmrcod@gmail.com>
2021-02-13 13:32:29 -0600

    i just learned how java works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InitialLifterCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InitialLifterCode.java
@@ -13,32 +13,30 @@ public class InitialLifterCode extends LinearOpMode {
     private CRServo ServoPower;
     private double LeftStickValue;
     private double RightStickValue;
-    private double LiftPower;
-    private double ForkPower;
-
 
     private class Lifter {
-        private void MoveLift(double LeftStick, double InLiftPower) {
+        private double LiftPower;
+        private double ForkPower;
+        private void MoveLift(double LeftStick) {
             if (LeftStick == 0) {
                 LiftMotor.setPower(0.07);
-                InLiftPower = 0.07;
+                this.LiftPower = 0.07;
             }
 
             if (LeftStick < 0) {
                     LiftMotor.setPower(LeftStick * 0.46);
-                    InLiftPower = LeftStick * 0.46;
+                    this.LiftPower = LeftStick * 0.46;
                 } else {
                     LiftMotor.setPower(LeftStick);
-                    InLiftPower = LeftStick;
+                    this.LiftPower = LeftStick;
                 }
             }
 
-        public void MoveServo(double RightStick, double InForkPower) {
+        public void MoveServo(double RightStick) {
             ServoPower.setPower(RightStick);
-            InForkPower = RightStick;
+            this.ForkPower = RightStick;
         }
     }
-
 
     @Override
     public void runOpMode() {
@@ -46,17 +44,15 @@ public class InitialLifterCode extends LinearOpMode {
         expansion_Hub_1 = hardwareMap.get(Blinker.class, "Nihal");
         LiftMotor = hardwareMap.get(DcMotor.class, "Big Motor");
         ServoPower = hardwareMap.get(CRServo.class, "ServoPower");
-        telemetry.addData("Lift Power", LiftPower);
-        telemetry.addData("Fork Power", ForkPower);
+        telemetry.addData("Lift Power", lift.LiftPower);
+        telemetry.addData("Fork Power", lift.ForkPower);
         waitForStart();
         while (opModeIsActive()) {
             LeftStickValue = -gamepad1.left_stick_y;
             RightStickValue = -gamepad1.right_stick_y;
+            lift.MoveLift(LeftStickValue);
+            lift.MoveServo(RightStickValue);
             telemetry.update();
-            lift.MoveLift(LeftStickValue, LiftPower);
-            lift.MoveServo(RightStickValue, ForkPower);
         }
-
-
     }
 }
