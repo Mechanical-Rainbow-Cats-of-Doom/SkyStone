7bdb6c99f68e6dd3d4a1ebd6ac1ba27cfd60a7eb
Ethan Sanders <ethanmrcod@gmail.com>
2021-02-13 14:31:19 -0600

    i am very smart and know everything about java in general ahhasdhsdhajhdas

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InitialLifterCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InitialLifterCode.java
@@ -19,16 +19,16 @@ public class InitialLifterCode extends LinearOpMode {
         private double ForkPower;
         private void MoveLift(double LeftStick) {
             if (LeftStick == 0) {
-                LiftMotor.setPower(-0.08);
-                this.LiftPower = -0.08;
+                LiftMotor.setPower(-0.1);
+                this.LiftPower = -0.1;
             }
 
             if (LeftStick < 0) {
                     LiftMotor.setPower(LeftStick * -0.46);
-                    this.LiftPower = LeftStick * -0.46;
+                    this.LiftPower = (LeftStick * -0.46);
                 } else {
-                    LiftMotor.setPower(-LeftStick);
-                    this.LiftPower = -LeftStick;
+                    LiftMotor.setPower(LeftStick * -0.8);
+                    this.LiftPower = (LeftStick * -0.8);
                 }
             }
 
@@ -44,14 +44,14 @@ public class InitialLifterCode extends LinearOpMode {
         expansion_Hub_1 = hardwareMap.get(Blinker.class, "Nihal");
         LiftMotor = hardwareMap.get(DcMotor.class, "Big Motor");
         ServoPower = hardwareMap.get(CRServo.class, "ServoPower");
-        telemetry.addData("Lift Power", lift.LiftPower);
-        telemetry.addData("Fork Power", lift.ForkPower);
         waitForStart();
         while (opModeIsActive()) {
             LeftStickValue = -gamepad1.left_stick_y;
             RightStickValue = -gamepad1.right_stick_y;
             lift.MoveLift(LeftStickValue);
             lift.MoveServo(RightStickValue);
+            telemetry.addData("Lift Power", lift.LiftPower);
+            telemetry.addData("Fork Power", lift.ForkPower);
             telemetry.update();
         }
     }
