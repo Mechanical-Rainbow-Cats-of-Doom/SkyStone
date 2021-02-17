83fe292da301b8107e5d49d8dbaeb749a9496caf
ProfessorPiggos <doawk7@gmail.com>
2021-02-16 20:24:35 -0600

    removed even more stuff that annoyed me

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwentyTwentyOneOpModeCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwentyTwentyOneOpModeCode.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
-
 import android.os.DropBoxManager;
-import com.qualcomm.hardware.bosch.BNO055IMU;  //This is the package for controlling the IMU
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Blinker;
@@ -12,27 +11,25 @@ import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-
-import java.lang.Math;  //This is the standard Java package for a variety of math functions
+import java.lang.Math;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-
 @TeleOp
-
 //Merging, lift, and part of intake done by Ethan, the rest done by Nihal.
 //back right front right front left back left
 public class TwentyTwentyOneOpModeCode extends LinearOpMode {
+    
     private double LeftStickValue;
     private double RightStickValue;
     private Blinker Control_Hub;
     private Blinker expansion_Hub_2;
     ElapsedTime mytimer = new ElapsedTime();
+    
     @Override
     public void runOpMode() {
         InitialLauncherAndIntakeCode.Launcher launcher = new InitialLauncherAndIntakeCode.Launcher();
