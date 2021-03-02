e9b3e3ba89c0850254008e1236c1ad31387a7d5b
Ethan Sanders <ethanmrcod@gmail.com>
2021-03-01 18:29:10 -0600

    added distance sensor program
    Signed-off-by: Ethan Sanders <ethanmrcod@gmail.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSensorTesting.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+import android.os.DropBoxManager;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Blinker;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import java.lang.Math;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+@TeleOp
+
+public class DistanceSensorTesting extends LinearOpMode {
+    private OpticalDistanceSensor DistanceSensor;
+    @Override
+    public void runOpMode() {
+        waitForStart();
+        DistanceSensor = hardwareMap.get(OpticalDistanceSensor.class, "Distance Sensor");
+        while (opModeIsActive()) {
+        telemetry.addData("Distance Sensor", DistanceSensor);
+        telemetry.update();
+        }
+    }
+}
