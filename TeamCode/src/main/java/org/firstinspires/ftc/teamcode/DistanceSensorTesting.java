4995a1ea292aeb6d16f979b4b43b76d11cd172ee
Ethan Sanders <ethanmrcod@gmail.com>
2021-03-01 19:44:01 -0600

    Added an array buffer to in theory smooth out value reporting
    Signed-off-by: Ethan Sanders <ethanmrcod@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSensorTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSensorTesting.java
@@ -17,20 +17,42 @@ import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import java.lang.Math;
+import java.lang.reflect.Array;
+
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 @TeleOp
 
 public class DistanceSensorTesting extends LinearOpMode {
-    private OpticalDistanceSensor DistanceSensor;
+    private DistanceSensor DistanceSensor;
+
     @Override
     public void runOpMode() {
+        DistanceSensor = hardwareMap.get(DistanceSensor.class, "Distance Sensor");
+        double AveragedArray;
+        int i;
+        double total = 0;
+        int index = 0;
+        int ArraySize = 10;
+        double [] sensorArray;
+        sensorArray = new double[ArraySize];
         waitForStart();
-        DistanceSensor = hardwareMap.get(OpticalDistanceSensor.class, "Distance Sensor");
         while (opModeIsActive()) {
-        telemetry.addData("Distance Sensor", DistanceSensor);
-        telemetry.update();
+            sensorArray[index] = DistanceSensor.getDistance(DistanceUnit.MM);
+            if (index >= (ArraySize - 1)) {
+                index = 0;
+            }
+            else {
+                index++;
+            }
+            for (i = 0; i<sensorArray.length; i++) {
+                total = total + sensorArray[i];
+            }
+            AveragedArray = total / sensorArray.length;
+            telemetry.addData("Average Array", AveragedArray);
+            telemetry.update();
         }
     }
 }
