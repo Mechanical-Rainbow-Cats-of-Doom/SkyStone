d3eec81367717eca590e669540a59ebe2cee089f
Ethan Sanders <ethanmrcod@gmail.com>
2021-04-03 13:11:21 -0500

    ahh my code doesn't work. whatever.
    Signed-off-by: Ethan Sanders <ethanmrcod@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InitialLauncherAndIntakeCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InitialLauncherAndIntakeCode.java
@@ -21,7 +21,6 @@ public class InitialLauncherAndIntakeCode {
         }
 
         public void LauncherRun(double power) {
-            if (power == null) { power == 1 }
             if (launcherOn && power <= 1 && power >= -1) {
                 power = power * -maxPower;
                 LaunchMotor.setPower(power);
