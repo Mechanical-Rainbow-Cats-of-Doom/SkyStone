c00ab854ac3b295f62938f9960e351140da19320
Ethan Sanders <ethanmrcod@gmail.com>
2021-04-12 19:16:51 -0500

    scrubbed even more of the fork servo
    Signed-off-by: Ethan Sanders <ethanmrcod@gmail.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LifterCode.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+//Created by Ethan Sanders
+public class LifterCode {
+    public static class Lifter {
+        public double LiftPower;
+        public double ForkPower;
+        public DcMotor LiftMotor;
+
+        public void MoveLift(double LeftStick) {
+            if (LeftStick == 0) {
+                this.LiftMotor.setPower(-0.1);
+                this.LiftPower = -0.1;
+            } else if (LeftStick < 0) {
+                this.LiftMotor.setPower(LeftStick * -0.46);
+                this.LiftPower = (LeftStick * -0.46);
+            } else {
+                this.LiftMotor.setPower(-LeftStick);
+                this.LiftPower = (LeftStick * -1);
+            }
+        }
+    }
+}
